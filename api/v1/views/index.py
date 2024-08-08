3b8a60c387fc3412b59352ac2bff5242e20fb6c8
enochCodes <henoksamson57@gmail.com>
2024-08-08 18:26:34 +0300

    fix(api/app.py): docmented app.py index.pyu

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    index
+Index module for the Flask web application.
 """
 from flask import jsonify
 from api.v1.views import app_views
@@ -9,6 +9,6 @@ from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """
-        return status
+    Get the status of the API.
     """
     return jsonify({"status": "OK"})
