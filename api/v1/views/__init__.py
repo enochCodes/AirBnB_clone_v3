d8b918f3e27443cbda53242c2f655c1f36449c8e
enochCodes <henoksamson57@gmail.com>
2024-08-08 22:02:00 +0300

    fix(bug): error

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python3
-"""
-Initialize the views for the Flask web application.
-"""
-
 from flask import Blueprint
-from index import *
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.index import *
