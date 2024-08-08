b34fe8ba0a0dc037cbd33ef1c21d34986dabc913
enochCodes <henoksamson57@gmail.com>
2024-08-08 18:20:13 +0300

    fix(api/app.py): docmented app.py index.pyu

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Srart Api"""
+"""
+Flask web application entry point.
+"""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -13,12 +15,15 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown(exception):
     """
-        teardown
+      Handle teardown of the application context.
     """
     storage.close()
 
 
 if __name__ == "__main__":
+    """
+    Main entry point for running the Flask application.
+    """
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
