b76c56b9d6b4fc0695c7d24f00f147ee2a8c9670
enochCodes <henoksamson57@gmail.com>
2024-08-09 16:00:05 +0300

    feat(add-api-endpoint): for stats add get-stats api endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,18 @@ from api.v1.views import app_views
 def returnstuff():
     '''return stuff'''
     return jsonify(status='OK')
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """
+    Retrieves the number of each object by type
+    """
+    stats = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(stats)
