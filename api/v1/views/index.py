c0a40ff15b882cd0ca2a5e22449529642cd04377
enochCodes <henoksamson57@gmail.com>
2024-08-09 18:42:30 +0300

    feat(add-api-endpoint): for stats add get-stats api endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,16 @@
-#!/usr/bin/python3xx
+#!/usr/bin/python3
 '''api status'''
 import models
 from models import storage
 from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
-
+from models.amenity import Amenity
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
 
 @app_views.route('/status', strict_slashes=False)
 def returnstuff():
