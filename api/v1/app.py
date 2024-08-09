c74e7ec31dab63056816d29480c08a6d291c7642
enochCodes <henoksamson57@gmail.com>
2024-08-09 23:49:58 +0300

    feat(add-error-page-404): 404 error page

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,12 @@ def downtear(self):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST')
     port = getenv('HBNB_API_PORT')
